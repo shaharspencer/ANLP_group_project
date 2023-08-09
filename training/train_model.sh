6f7a3bb35be9011ca6635e1c81cca9b7d79bce68
niveck <niv.eckhaus@gmail.com>
2023-08-09 13:11:59 +0300

    made bash script for t5-small

--- training/train_model.sh
+++ training/train_model.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#SBATCH --gres=gpu:2 #request gpu resources
+#SBATCH --gres=gpu:a5000:1 #request gpu resources
+#SBATCH --mem=32G
 #SBATCH --nodes=1 # request nodes
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1 #request cpu resources
@@ -14,17 +15,21 @@ source /cs/snapless/gabis/shaharspencer/anlp_project_venv/bin/activate
 
 module load cuda/11.7
 
-python run_summarization.py \
+python /cs/snapless/gabis/shaharspencer/ANLP_group_project/prepare.py
+
+python /cs/snapless/gabis/shaharspencer/ANLP_group_project/training/run_summarization.py \
     --model_name_or_path t5-small \
     --do_train \
     --do_eval \
-    --train_file ../data_files/abstracts_introductions_train_split.csv \
-    --validation_file ../data_files/abstracts_introductions_validation_split.csv \
+    --do_predict \
+    --train_file /cs/snapless/gabis/shaharspencer/ANLP_group_project/data_files/neural_net/abstracts_introductions_train_split.csv \
+    --validation_file /cs/snapless/gabis/shaharspencer/ANLP_group_project/data_files/neural_net/abstracts_introductions_validation_split.csv \
+    --test_file /cs/snapless/gabis/shaharspencer/ANLP_group_project/data_files/neural_net/abstracts_introductions_test_split.csv \
     --source_prefix "summarize: " \
-    --output_dir trained_t5_small_model \
-    --overwrite_output_dir \
+    --output_dir /cs/snapless/gabis/shaharspencer/ANLP_group_project/model_outputs/trained_t5_small_model_neural_net \
+    --overwrite_output_dir=True \
     --per_device_train_batch_size=4 \
     --per_device_eval_batch_size=4 \
     --predict_with_generate \
-     --text_column introduction \
+    --text_column introduction \
     --summary_column abstract
