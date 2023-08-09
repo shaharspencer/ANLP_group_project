ab6c28bf71ed63f90350c5c274b96f30c86bd705
niveck <niv.eckhaus@gmail.com>
2023-08-09 23:04:26 +0300

    fix in paths

--- model_scripts/prediction/predict_on_TRAIN_noisy_label_TEST_noisy_label_model.sh
+++ model_scripts/prediction/predict_on_TRAIN_noisy_label_TEST_noisy_label_model.sh
@@ -23,14 +23,14 @@ cd /cs/snapless/gabis/shaharspencer/ANLP_group_project/
 python prepare.py
 
 python model_scripts/training/run_summarization.py \
-    --model_name_or_path model_outputs/trained_t5_large_model_GENERIC \
-    --config_name model_outputs/trained_t5_large_model_GENERIC/config.json \
-    --training_dataset_name GENERIC \
-    --prediction_dataset_name GENERIC \
+    --model_name_or_path model_outputs/trained_t5_large_model_neural_net \
+    --config_name model_outputs/trained_t5_large_model_neural_net/config.json \
+    --training_dataset_name noisy_label \
+    --prediction_dataset_name noisy_label \
     --do_predict \
-    --test_file data_files/GENERIC/abstracts_introductions_test_split.csv \
+    --test_file data_files/noisy_label/abstracts_introductions_test_split.csv \
     --source_prefix "summarize: " \
-    --output_dir model_outputs/trained_t5_large_model_GENERIC \
+    --output_dir model_outputs/trained_t5_large_model_neural_net \
     --per_device_train_batch_size=2 \
     --per_device_eval_batch_size=2 \
     --predict_with_generate \
