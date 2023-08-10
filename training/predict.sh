aad07cab1461da736a14a30df06b256e8272155c
Shahar Spencer <shahar.spencer@mail.huji.ac.il>
2023-08-10 03:04:00 +0300

    changed predict for RP/NL

--- training/predict.sh
+++ training/predict.sh
@@ -15,12 +15,11 @@ module load cuda/11.7
 
 python /cs/snapless/gabis/shaharspencer/ANLP_group_project/prepare.py
 
-trained_t5_large_model_routing_protocols
 python /cs/snapless/gabis/shaharspencer/ANLP_group_project/training/run_summarization.py \
     --config_name /cs/snapless/gabis/shaharspencer/ANLP_group_project/model_outputs/trained_t5_large_model_routing_protocols/config.json \
     --model_name_or_path /cs/snapless/gabis/shaharspencer/ANLP_group_project/model_outputs/trained_t5_large_model_routing_protocols \
     --do_predict \
-    --test_file /cs/snapless/gabis/shaharspencer/ANLP_group_project/data_files/neural_network_verification/abstracts_introductions_test_split.csv \
+    --test_file /cs/snapless/gabis/shaharspencer/ANLP_group_project/data_files/noisy_label/abstracts_introductions_test_split.csv \
     --source_prefix "summarize: " \
     --output_dir /cs/snapless/gabis/shaharspencer/ANLP_group_project/model_outputs/trained_t5_large_model_routing_protocols \
     --per_device_train_batch_size=2 \
